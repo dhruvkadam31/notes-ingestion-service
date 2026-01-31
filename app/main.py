225fdf9825713c38c4f0d66e4ac9408ff66a0067
dhruvkadam31 <dhruvkadam31yt@gmail.com>
2026-01-31 17:00:01 +0530

    Add basic text cleaning logic

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from app.cleaning.cleaner import clean_text
 
 app = FastAPI()
 
@@ -9,3 +11,14 @@ def root():
 @app.get("/health")
 def health_check():
     return {"status":"ok"}
+
+class TextInput(BaseModel):
+    text: str 
+
+@app.post("/upload/text")  
+def upload_text(data: TextInput): 
+    cleaned = clean_text(data.text)
+    return {
+        "raw_text": data.text,
+        "cleaned_text":cleaned
+        } 
